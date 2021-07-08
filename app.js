39cd4816fc9246bd24a61606f92aebcc1f7871c8
robingiraldo123 <87104809+robingiraldo123@users.noreply.github.com>
2021-07-07 21:28:48 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
-// Crear una función que me permita determinar
-// la calificación de 3 alumnos
-// uno con nota de 100, otro con nota de 80
-// y el último con nota de 59
+function imprimirTabla(base, limite) {
 
+    for (let i = 1; i <= limite; i++) {
 
-function calcularNota(nota) {
+        let resultado = base * i;
 
-    let notaLetra = '';
+        console.log(base + ' x ' + i + ' = ' + resultado);
 
-    if (nota >= 90) {
-        notaLetra = 'A';
-    } else if (nota >= 80) {
-        notaLetra = 'B';
-    } else if (nota >= 70) {
-        notaLetra = 'C';
-    } else if (nota >= 60) {
-        notaLetra = 'D';
-    } else {
-        notaLetra = 'F';
     }
 
-    console.log(nota + ' es igual a ' + notaLetra);
 }
 
-
-calcularNota(100);
-calcularNota(80);
-calcularNota(59);
+imprimirTabla(10, 5);
+imprimirTabla(3, 10);
